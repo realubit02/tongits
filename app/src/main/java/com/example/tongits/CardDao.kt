45874ab9704930fd9399eacb3888b651d3d44812
jm <staffitjm790@gmail.com>
2023-08-06 18:09:25 +0800

    2nd commit

--- app/src/main/java/com/example/tongits/CardDao.kt
+++ app/src/main/java/com/example/tongits/CardDao.kt
@@ -1,18 +1,3 @@
 package com.example.tongits
 
-import androidx.annotation.DrawableRes
-import androidx.room.Dao
-import androidx.room.Entity
-import androidx.room.Insert
-import androidx.room.PrimaryKey
-import androidx.room.Query
-
-//
-//@Dao
-//interface CardDao {
-//    @Insert
-//    suspend fun insert(card: Card)
-//
-//    @Query("SELECT * FROM cards")
-//    suspend fun getAllCards(): List<Card>
-//}
+data class CardDao(val cardImageResource: Int)
